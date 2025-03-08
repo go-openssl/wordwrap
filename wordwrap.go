4c3f17256d672ece73c61f17c50d60839e61e905
wangben <wangben-1203@qq.com>
2025-03-08 15:39:41 +0800

    转换成公私钥

--- wordwrap.go
+++ wordwrap.go
@@ -4,8 +4,8 @@ import (
 	"strings"
 )
 
-// Wrap wraps a string into multiple lines, with a given width.
-func Wrap(text string, width int) string {
+// convert converts the given text to a multi-line string with the specified width.
+func convert(text string, width int) string {
 	var lines []string
 	runes := []rune(text)
 	currentLine := ""
@@ -25,10 +25,25 @@ func Wrap(text string, width int) string {
 	return strings.Join(lines, "\n")
 }
 
-func ToSslText(text string) string {
+type Wordwrap struct {
+}
+
+func New() *Wordwrap {
+	return &Wordwrap{}
+}
+
+func (w *Wordwrap) ToPrivateKey(text string) string {
 	str := "-----BEGIN RSA PRIVATE KEY-----\n"
-	str += Wrap(text, 64)
+	str += convert(text, 64)
 	str += "\n-----END RSA PRIVATE KEY-----"
+
+	return str
+}
+
+func (w *Wordwrap) ToPublicKey(text string) string {
+	str := "-----BEGIN PUBLIC KEY-----\n"
+	str += convert(text, 64)
+	str += "\n-----END PUBLIC KEY-----"
 
 	return str
 }
